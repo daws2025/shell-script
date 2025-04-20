4efd3b369beddfdccc1c68748be154c08ac87e2b
daws2025 <devsecopswithaws@gmail.com>
2025-04-21 03:43:37 +0530

    CHANGE

---
+++ loops.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+USERID=$(id -u)
+VALIDATE()
+if [ $1 -ne 0 ]
+then 
+    echo "$2 is failure"
+else
+    echo "$2 is sucess"
+fi
+if [ $USERID -ne 0 ]
+then 
+    echo "you do not have sudo access"
+    exit 1
+fi
+for package in $@
+do
+    dnf list installed $package
+        if [ $? -ne 0 ]
+        then
+            dnf install $package
+            VALIDATE $? Installing $package
+        else
+            echo "$package already installed"
+        fi
+done
+
+
+
+
